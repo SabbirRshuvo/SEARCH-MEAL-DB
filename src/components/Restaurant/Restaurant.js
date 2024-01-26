adda9c4d27aa4b930aadf467c9e575569daac900
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-01-26 11:21:43 +0600

    second commit

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -3,15 +3,17 @@ import Meal from '../Meal/Meal';
 import './Restaurant.css'
 
 const Restaurant = () => {
+
     const [searchText, setSearchText] = useState('');
+
     const [meals, setMeals] = useState([]);
 
-    useEffect( () =>{
-        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    useEffect(() => {
+        const url= (`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`);
         fetch(url)
         .then(res => res.json())
         .then(data => setMeals(data.meals));
-    }, [searchText])
+    },[searchText])
 
     const searchFood = e =>{
         setSearchText(e.target.value);
@@ -19,10 +21,11 @@ const Restaurant = () => {
     return (
         <div>
             <h2>Find the food you want</h2>
-            <input onChange={searchFood} type="text" name="" id="" />
+            <input onChange={searchFood} type="text" />
             <br />
-            <h3>Result found: {meals.length}</h3>
-            <div className='meals-container'>
+            <h3>Result Found : {meals.length}</h3>
+
+            <div>
                 {
                     meals.map(meal=> <Meal key={meal.idMeal} meal={meal}></Meal>)
                 }
